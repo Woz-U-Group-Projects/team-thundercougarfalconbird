d7d45d2a1549415a386f2f94fc3b89bf122f2f66
Hayden <hayden.ravert@gmail.com>
2019-07-01 19:09:13 -0400

    added sql and sequelize

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Password1!',
+  database: 'sakila'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log('Yay! You are connected to the database!');
+})
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
