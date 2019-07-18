020dd28d9f7f163ba266ee7f89d9e1219e9e247c
Hayden <hayden.ravert@gmail.com>
2019-07-18 12:26:58 -0400

    created product components and service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,10 +9,33 @@ import { SignupComponent } from './signup/signup.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ProductviewComponent } from './productview/productview.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { AdminComponent } from './admin/admin.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignupComponent, HeaderComponent, HomeComponent, ProfileComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent, 
+    LoginComponent, 
+    SignupComponent, 
+    HeaderComponent, 
+    HomeComponent, 
+    ProfileComponent, 
+    ProductviewComponent, 
+    InventoryComponent, 
+    AdminComponent, 
+    WishlistComponent, 
+    ShoppingcartComponent, ProductComponent
+  ],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    HttpClientModule, 
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
